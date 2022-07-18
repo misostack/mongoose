4b72b6335d0956d43ac8ce02f189503692ad777c
misostack <misostack.com@gmail.com>
2022-07-18 16:04:19 +0700

    add sample data validation

--- schemas/example.schema.js
+++ schemas/example.schema.js
@@ -3,10 +3,18 @@ const { Schema } = mongoose;
 
 const exampleSchema = new Schema(
   {
-    title: String,
-    content: String,
-    description: String,
-    slug: String,
+    title: { type: String, required: true, maxLength: 75 },
+    content: { type: String, required: true },
+    description: { type: String, required: true, maxLength: 160 },
+    slug: { type: String, required: true },
+    status: {
+      type: String,
+      enum: {
+        values: ["pending", "published", "archived"],
+      },
+      required: false,
+      default: "pending",
+    },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
   },
--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 const { Example } = require("./schemas/index.js");
+const { v4 } = require("uuid");
 
 const main = async () => {
   await mongoose.connect("mongodb://localhost:27017/mongoose");
   // test create new example
-  const example = new Example({ title: "Test" });
+  const example = new Example({
+    title: "Test",
+    description: "lorem ipsum",
+    content: "favorite content",
+    slug: v4(),
+  });
   example.save((error, doc) => {
     if (error) {
       throw new Error(error);
